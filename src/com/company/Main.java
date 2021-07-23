49c117c39d81966ddfc7d4ae4956e361040a22fa
EugeneMatyshenko <eugene.matyshenko@gmail.com>
2021-07-23 11:28:10 +0300

    Learn#1:Added date display for the main method.This is a combination of 4 commits.

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
